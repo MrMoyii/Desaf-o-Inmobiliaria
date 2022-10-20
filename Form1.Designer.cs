f61290e400f16750fbe6529c07c8c6c7e992b08b
MrMoyii <omarmoyano7w@gmail.com>
2022-10-20 13:40:51 -0300

    vista

---
+++ Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿
+namespace Desafio_Herencia_1_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtM2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPiso = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtVentanas = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrecioBase = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPrecioCalculado = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.Nuevo = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.LocalComercial = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(291, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese la dirección:";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(294, 32);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(180, 20);
+            this.txtDireccion.TabIndex = 1;
+            // 
+            // txtM2
+            // 
+            this.txtM2.Location = new System.Drawing.Point(294, 82);
+            this.txtM2.Name = "txtM2";
+            this.txtM2.Size = new System.Drawing.Size(180, 20);
+            this.txtM2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese la cantidad de m2:";
+            // 
+            // txtPiso
+            // 
+            this.txtPiso.Location = new System.Drawing.Point(294, 231);
+            this.txtPiso.Name = "txtPiso";
+            this.txtPiso.Size = new System.Drawing.Size(178, 20);
+            this.txtPiso.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ingrese el número de piso:";
+            // 
+            // txtVentanas
+            // 
+            this.txtVentanas.Location = new System.Drawing.Point(294, 274);
+            this.txtVentanas.Name = "txtVentanas";
+            this.txtVentanas.Size = new System.Drawing.Size(178, 20);
+            this.txtVentanas.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(291, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ingrese el número de ventanas:";
+            // 
+            // txtPrecioBase
+            // 
+            this.txtPrecioBase.Location = new System.Drawing.Point(294, 342);
+            this.txtPrecioBase.Name = "txtPrecioBase";
+            this.txtPrecioBase.Size = new System.Drawing.Size(178, 20);
+            this.txtPrecioBase.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(291, 325);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Precio base:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(291, 297);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(187, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "______________________________";
+            // 
+            // txtPrecioCalculado
+            // 
+            this.txtPrecioCalculado.Location = new System.Drawing.Point(294, 443);
+            this.txtPrecioCalculado.Name = "txtPrecioCalculado";
+            this.txtPrecioCalculado.ReadOnly = true;
+            this.txtPrecioCalculado.Size = new System.Drawing.Size(178, 20);
+            this.txtPrecioCalculado.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(291, 426);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Precio calculado:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.Nuevo);
+            this.panel1.Location = new System.Drawing.Point(294, 130);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 22);
+            this.panel1.TabIndex = 18;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(117, 2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Usado";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Nuevo
+            // 
+            this.Nuevo.AutoSize = true;
+            this.Nuevo.Checked = true;
+            this.Nuevo.Location = new System.Drawing.Point(4, 2);
+            this.Nuevo.Name = "Nuevo";
+            this.Nuevo.Size = new System.Drawing.Size(104, 17);
+            this.Nuevo.TabIndex = 0;
+            this.Nuevo.TabStop = true;
+            this.Nuevo.Text = "Nuevo(-15 años)";
+            this.Nuevo.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.LocalComercial);
+            this.panel2.Controls.Add(this.radioButton3);
+            this.panel2.Location = new System.Drawing.Point(294, 180);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(180, 22);
+            this.panel2.TabIndex = 19;
+            // 
+            // LocalComercial
+            // 
+            this.LocalComercial.AutoSize = true;
+            this.LocalComercial.Location = new System.Drawing.Point(78, 3);
+            this.LocalComercial.Name = "LocalComercial";
+            this.LocalComercial.Size = new System.Drawing.Size(100, 17);
+            this.LocalComercial.TabIndex = 3;
+            this.LocalComercial.Text = "Local Comercial";
+            this.LocalComercial.UseVisualStyleBackColor = true;
+            this.LocalComercial.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(4, 3);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(45, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Piso";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(291, 114);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(171, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Seleccione el estado del inmueble:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(291, 164);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(176, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Que tipo de inmuebles desea tasar?";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(291, 394);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(187, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "______________________________";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 507);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPrecioCalculado);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtPrecioBase);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtVentanas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPiso);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtM2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtM2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPiso;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtVentanas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrecioBase;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPrecioCalculado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton Nuevo;
+        private System.Windows.Forms.RadioButton LocalComercial;
+        private System.Windows.Forms.Label label10;
+        public System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.Panel panel2;
+        public System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using Desafio_Herencia_1_.presenter;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio_Herencia_1_
+{
+    public partial class Form1 : Form, IView
+    {
+        Presentador presentador;
+        public Form1()
+        {
+            InitializeComponent();
+            txtPiso.Enabled = true;
+            txtVentanas.Enabled = false;
+        }
+
+        //actualiza los valores del display 
+        public void DisplayResult(string result, bool error, string quePaso)
+        {
+            //si hay un error lo muestro
+            if (error)
+                MessageBox.Show(quePaso, result);
+            else
+                txtPrecioCalculado.Text = result;
+        }
+
+        //da los valores del input
+        public ArrayList GetInputValues()
+        {
+            //retora los valores de los input
+            ArrayList arr = new ArrayList();
+            arr.Add(txtDireccion.Text);      //0                 obligatorio validiar
+            arr.Add(txtM2.Text);             //1                 obligatorio validiar
+            arr.Add(txtPrecioBase.Text);     //2 precioBase      obligatorio validiar
+            arr.Add(txtPiso.Text);           //3                 este campo se valida si se eligio piso
+            arr.Add(txtVentanas.Text);       //4                 este campo se valida si se eligio local comercial
+            arr.Add(Nuevo.Checked);          //5
+            arr.Add(LocalComercial.Checked); //6
+            return arr;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            presentador = new Presentador(this);
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPiso.Enabled = true;
+            txtVentanas.Enabled = false;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPiso.Enabled = false;
+            txtVentanas.Enabled = true;
+        }
+    }
+}
